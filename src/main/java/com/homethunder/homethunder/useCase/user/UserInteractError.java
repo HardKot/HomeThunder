811f78a6ff8a762a7edd14274c951122b712e8c7
Nikita Batyukhno <nikita@batyukhno-dev.ru>
2024-04-15 20:18:22 +0500

    migrate to repomodule

--- src/main/java/com/homethunder/homethunder/useCase/user/UserInteractError.java
+++
@@ -1,8 +0,0 @@
-package com.homethunder.homethunder.useCase.user;
-
-public sealed class UserInteractError {
-
-    public static final class PasswordNotConfirm extends UserInteractError {  }
-
-    public static final class EmailExists extends UserInteractError {}
-}
--- src/main/resources/db/migration/V20240414__token_storage.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE jwt
-(
-    id         UUID PRIMARY KEY   default gen_random_uuid(),
-    user_id    UUID      NOT NULL REFERENCES app_users (id),
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    device_name VARCHAR
-);
