ae8d75ab50db272b6f3dbd8f13862cb1b1e896c4
Nikita Batyukhno <nikita@batyukhno-dev.ru>
2024-04-19 09:28:58 +0500

    change middleware

---
+++ www/middleware.ts
@@ -0,0 +1,6 @@
+import {NextRequest} from "next/server";
+import {authenticationMiddleware} from "@/app/middleware/AuthenticationMiddleware";
+
+export default function middleware(req: NextRequest) {
+  return authenticationMiddleware(req)
+}
--- www/src/app/middleware/AuthenticationMiddleware.ts
+++ www/src/app/middleware/AuthenticationMiddleware.ts
@@ -1,8 +1,29 @@
 import {NextRequest, NextResponse} from "next/server";
 import {AuthManager} from "@/shared/api/AuthManager";
+import {RuleRouting} from "@/app/RuleRouting";
 
 export const authenticationMiddleware = (request: NextRequest) => {
-  if (!AuthManager.build().getToken()) return NextResponse.redirect("/login");
-  if (request.nextUrl.pathname.startsWith("/login") || request.nextUrl.pathname.startsWith("/registration")) return NextResponse.redirect("/")
-  return NextResponse.next();
+  const authManager = AuthManager.build();
+  const token = authManager.getToken()
+  if (!token && (
+    !request.nextUrl.pathname.startsWith("/registration") ||
+    !request.nextUrl.pathname.startsWith("/login") ||
+    !request.nextUrl.pathname.startsWith("/login")
+  )) return NextResponse.redirect("/login");
+
+
+  if (request.nextUrl.pathname.startsWith("/registration") ||
+    request.nextUrl.pathname.startsWith("/login") ||
+    request.nextUrl.pathname.startsWith("/login")
+  ) return NextResponse.redirect("/")
+
+  const rules = RuleRouting(request.nextUrl.pathname);
+
+  if (!rules.length) return NextResponse.next();
+
+  for (const rule of rules) {
+    if (authManager.hasAccess(rule)) return NextResponse.next();
+  }
+
+  return NextResponse.redirect(request.headers.get("referer") ?? "/")
 }
